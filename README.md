cd06607f9c580df7eaa4819a6e524deef9424be0
herrkaefer <gloolar@gmail.com>
2017-03-30 21:39:32 +0800

    first

---
+++ README.md
@@ -0,0 +1,17 @@
+Script for Shadowsocks server setup on Ubuntu VPS
+
+## How to use
+
+On VPS,
+
+```
+wget https://github.com/herrkaefer/sssetup/master/sssetup.sh
+chmod +x sssetup.sh
+```
+Edit sssetup.sh, Change password or other settings (line 16 ~ 22), then 
+```
+./sssetup.sh
+```
+
+In the process, press any key according to the prompt, and reboot.
+
---
+++ sssetup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-m2crypto
+sudo apt-get install -y supervisor
+
+export LC_ALL=C
+
+pip install --upgrade pip
+pip install setuptools
+pip install shadowsocks
+
+cat >>/etc/shadowsocks.json<<EOF
+{
+    "server": "0.0.0.0",
+    "port_password": {
+        "8381": "ps1",
+        "8382": "ps2"
+    },
+    "timeout": 300,
+    "method": "aes-256-cfb"
+}
+EOF
+
+sudo ssserver -c /etc/shadowsocks.json -d start
+
+# sed '/exit 0/i\ssserver -c /etc/shadowsocks.json -d start' -i /etc/rc.local
+
+cat >>/etc/supervisor/conf.d/shadowsocks.conf<<EOF
+[program:shadowsocks]
+command=ssserver -c /etc/shadowsocks.json -d start
+autostart=true
+autorestart=true
+user=root
+log_stderr=true
+logfile=/var/log/shadowsocks.log
+EOF
+
+sudo service supervisor start
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+./bbr.sh
